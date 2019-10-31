8d7300f6c7f5306bc85018f712483f7f956702b3
Julian Peffer <jepeffer@unomaha.edu>
2019-10-30 21:09:59 -0500

    First Commit

---
+++ MVCWebServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCWebServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
