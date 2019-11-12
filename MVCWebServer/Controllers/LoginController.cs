45bfd562307d8736d20f99dae166c823731d68c8
jepeffer <49083083+jepeffer@users.noreply.github.com>
2019-11-12 10:02:58 -0600

    Update LoginController.cs

--- MVCWebServer/Controllers/LoginController.cs
+++ MVCWebServer/Controllers/LoginController.cs
@@ -1,13 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MVCWebServer.Models;
 
 namespace MVCWebServer.Controllers
 {
     public class LoginController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +20,59 @@ namespace MVCWebServer.Controllers
         public IActionResult NewUser()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CheckLogin(User userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MD5 md5Hash = MD5.Create())
+                {
+                    string hash = GetMd5Hash(md5Hash, userModel.Password);
+                }
+            }
+
+            return View();
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
